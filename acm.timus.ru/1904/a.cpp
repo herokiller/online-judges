ae85522be4fed47530c22ad22db96e7c77d8b653
Vladimir Li <vladimir.kbtu@gmail.com>
2013-06-13 12:16:03 +0600

    acm.timus.ru 1904

--- acm.timus.ru/1904/a.cpp
+++ acm.timus.ru/1904/a.cpp
@@ -9,7 +9,7 @@ struct mass {
 	int l, r;
 };
 
-bool b[100000];
+int b[100000];
 mass d[2][N];
 int n, a[N];
 
@@ -20,16 +20,29 @@ int main() {
 
 	int k = 1;	
 	d[0][1].l = a[n] - 1;
+	
 	d[0][1].r = a[n] + 1;
 
+	if ( n > 1 ) {
+		if (( d[0][1].l < 0 ) && ( d[0][1].r < 0))
+			k = 0;
+		else if ( d[0][1].l < 0 )
+			d[0][1].l = 0;
+		if ( d[0][1].r < 0 )
+			d[0][1].r = 0;
+	}
+	
 	for ( int i = n-1; i >= 1; i-- ) {
 		int p = 0;		
 		for ( int j = 1; j <= k; j++ ) {
 			p++;			
 			d[1][p].l = a[i] - d[0][j].r;
 			d[1][p].r = a[i] - d[0][j].l;
+
 			if ( i > 1 ) {
-				if ( d[1][p].l < 0 )
+				if (( d[1][p].l < 0 ) && ( d[1][p].r < 0)) 
+					p--;
+				else if ( d[1][p].l < 0 )
 					d[1][p].l = 0;
 				if ( d[1][p].r < 0 )
 					d[1][p].r = 0;
@@ -37,15 +50,18 @@ int main() {
 
 			p++;
 			d[1][p].l = a[i] + d[0][j].l;
-			d[1][p].r = a[i] + d[0][j].r;
+			d[1][p].r = a[i] + d[0][j].r;			
+
 			if ( i > 1 ) {
-				if ( d[1][p].l < 0 )
+				if (( d[1][p].l < 0 ) && ( d[1][p].r < 0)) 
+					p--;
+				else if ( d[1][p].l < 0 )
 					d[1][p].l = 0;
 				if ( d[1][p].r < 0 )
 					d[1][p].r = 0;
 			}
-
 		}
+
 		k = p;
 		for ( int j = 1; j <= k; j++ )
 			d[0][j] = d[1][j];
@@ -53,33 +69,51 @@ int main() {
     }
 
     for ( int i = 1; i <= k; i++ ) {
-    	for ( int j = d[0][i].l; j <= d[0][i].r; j++ ) 
-    		b[j+20000] = 1;
+
+    	if ( d[0][i].l == d[0][i].r ) {
+    		if ( b[d[0][i].l + 20000] == 0 )
+    			 b[d[0][i].l + 20000] = 1;
+    		continue;
+    	}
+
+    	if (b[d[0][i].l + 20000] == 3 )
+    		b[d[0][i].l + 20000] = 4;
+    	else if (b[d[0][i].l + 20000] < 2) 
+    		b[d[0][i].l + 20000] = 2;
+
+		if (b[d[0][i].r + 20000] == 2 )
+    		b[d[0][i].r + 20000] = 4;
+    	else if ( b[d[0][i].r + 20000] < 2 )
+    		b[d[0][i].r + 20000] = 3;
+
+    	for ( int j = d[0][i].l+1; j <= d[0][i].r-1; j++ ) 
+    		b[j+20000] = 4;
     }
 
     mass ans[10000];
     int nans = 0;
 
     for ( int i = 1; i <= 100000; i++ )
-		if ( b[i] ) {
-			k = i;
-			while ( b[k] )
+		if ( b[i] == 1 ) {
+			nans++;
+			ans[nans].l = i - 20000;
+			ans[nans].r = i - 20000;
+			continue;
+		} else if ( b[i] > 1 ) {
+			k = i+1;
+			while ( b[k] == 4 )
 				k++;
 
-			if ( k > i+1 ) {
-				nans++;
-				ans[nans].l = i - 20000;
-				ans[nans].r = k - 20001;
-				i = k-1;
-			}
+			nans++;
+			ans[nans].l = i - 20000;
+			ans[nans].r = k - 20000;
+			i = k;
 		}
 
 
 	printf("%d\n", nans);
 	for ( int i = 1; i <= nans; i++ )
 		printf("%d %d\n", ans[i].l, ans[i].r);
-
-
 
 	return 0;
 }
