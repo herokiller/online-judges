ae1b48ca65bacbb8795d4aa97555e783ff50b727
vli <vli@crystalspring.kz>
2015-07-29 09:56:58 +0600

    acm.timus.ru 2012

---
+++ acm.timus.ru/2012/a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+    int x;
+    scanf("%d", &x);
+    if ( x >= 7 )
+        printf("YES\n");
+    else
+        printf("NO\n");
+	return 0;
+}
+
